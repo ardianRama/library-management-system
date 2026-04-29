20e99cd8d3008f05a783a977f9222d7d36ba4cdb
Ardian Rama <ardian.rama@hotmail.se>
2026-04-29 17:37:57 +0200

    clean up

--- src/main/java/org/ardian/librarymanagementsystem/security/SecurityConfig.java
+++ src/main/java/org/ardian/librarymanagementsystem/security/SecurityConfig.java
@@ -19,7 +19,6 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults());
