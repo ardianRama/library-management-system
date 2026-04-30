2b1b21216bd685d545469ab360c62c69a5c20631
Ardian Rama <ardian.rama@hotmail.se>
2026-04-30 12:18:07 +0200

    removed annotation

--- src/main/java/org/ardian/librarymanagementsystem/security/SecurityConfig.java
+++ src/main/java/org/ardian/librarymanagementsystem/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package org.ardian.librarymanagementsystem.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,7 +12,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
  */
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
