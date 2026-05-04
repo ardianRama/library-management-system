2558c1e2dcb40c5d62a4ee6acfa88e1b7cea2b1f
Ardian Rama <ardian.rama@hotmail.se>
2026-05-04 15:34:01 +0200

    update LoanNotFoundException

--- src/main/java/org/ardian/librarymanagementsystem/exception/business/notfound/LoanNotFoundException.java
+++ src/main/java/org/ardian/librarymanagementsystem/exception/business/notfound/LoanNotFoundException.java
@@ -5,21 +5,18 @@ import lombok.Getter;
 @Getter
 public class LoanNotFoundException extends NotFoundException {
 
-    private final Long bookId;
-    private final Long loanId;
+    private final Long id;
     private final String email;
 
-    public LoanNotFoundException(Long bookId, String email) {
-        super("No active loan found for user " + email + " and bookId " + bookId);
-        this.bookId = bookId;
+    public LoanNotFoundException(Long id, String email) {
+        super("No active loan found for user " + email + " and bookId " + id);
+        this.id = id;
         this.email = email;
-        this.loanId = null;
     }
 
-    public LoanNotFoundException(Long loanId) {
-        super("No loan found with loanId  " + loanId);
-        this.loanId = loanId;
-        this.bookId = null;
+    public LoanNotFoundException(Long id) {
+        super("No loan found with loanId  " + id);
+        this.id = null;
         this.email = null;
     }
 }
