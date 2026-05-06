112bbdebd3f89fce50a98c2dff54bb4e31a38795
Ardian Rama <ardian.rama@hotmail.se>
2026-05-06 18:35:02 +0200

    update exception message for BookDeletion

--- src/main/java/org/ardian/librarymanagementsystem/exception/business/conflict/BookDeletionException.java
+++ src/main/java/org/ardian/librarymanagementsystem/exception/business/conflict/BookDeletionException.java
@@ -3,6 +3,6 @@ package org.ardian.librarymanagementsystem.exception.business.conflict;
 public class BookDeletionException extends ConflictException {
 
     public BookDeletionException() {
-        super("Book cannot be deleted because it is currently borrowed");
+        super("Book cannot be deleted because copies are currently on loan");
     }
 }
