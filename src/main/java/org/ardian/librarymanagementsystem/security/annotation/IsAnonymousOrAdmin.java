e55c000ecd134755735feed994c653e9e78e82ff
Ardian Rama <ardian.rama@hotmail.se>
2026-05-06 14:41:27 +0200

    add custom security annotation

--- src/main/java/org/ardian/librarymanagementsystem/security/annotation/IsAnonymousOrAdmin.java
+++ src/main/java/org/ardian/librarymanagementsystem/security/annotation/IsAnonymousOrAdmin.java
@@ -1,4 +1,19 @@
 package org.ardian.librarymanagementsystem.security.annotation;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Custom security annotation that grants access to anonymous users
+ * and administrators only.
+ */
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("isAnonymous() or hasRole('ADMIN')")
 public @interface IsAnonymousOrAdmin {
 }
