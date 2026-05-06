33147b144526a4ce3a479883ad480d5ccabbc513
Ardian Rama <ardian.rama@hotmail.se>
2026-05-06 18:00:16 +0200

    update message in exception

--- src/main/java/org/ardian/librarymanagementsystem/exception/business/notfound/BookNotFoundException.java
+++ src/main/java/org/ardian/librarymanagementsystem/exception/business/notfound/BookNotFoundException.java
@@ -3,6 +3,6 @@ package org.ardian.librarymanagementsystem.exception.business.notfound;
 public class BookNotFoundException extends NotFoundException {
 
     public BookNotFoundException() {
-        super("Book does not exist");
+        super("Book not found");
     }
 }
--- src/main/java/org/ardian/librarymanagementsystem/exception/business/notfound/LoanNotFoundException.java
+++ src/main/java/org/ardian/librarymanagementsystem/exception/business/notfound/LoanNotFoundException.java
@@ -3,6 +3,6 @@ package org.ardian.librarymanagementsystem.exception.business.notfound;
 public class LoanNotFoundException extends NotFoundException {
 
     public LoanNotFoundException() {
-        super("No loan found");
+        super("Loan not found");
     }
 }
