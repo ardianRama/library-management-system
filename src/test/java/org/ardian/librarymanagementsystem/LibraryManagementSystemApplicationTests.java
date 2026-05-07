31fcd837c035aa8c483e9b36be424bd81f48651b
Ardian Rama <ardian.rama@hotmail.se>
2026-05-07 14:53:29 +0200

    added Disabled annotation with explaining comment

--- src/test/java/org/ardian/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
+++ src/test/java/org/ardian/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -1,9 +1,11 @@
 package org.ardian.librarymanagementsystem;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@Disabled("Disabled in CI since contextLoads requires full Spring Boot initialization with database, which is not available in current test configuration")
+@SpringBootTest
 class LibraryManagementSystemApplicationTests {
 
     @Test
