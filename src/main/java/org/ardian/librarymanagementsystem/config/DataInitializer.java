a102c155ddc924187c0a552b42441d2bc78386a0
Ardian Rama <ardian.rama@hotmail.se>
2026-05-07 17:10:33 +0200

    Exclude DataInitializer from test profile

--- src/main/java/org/ardian/librarymanagementsystem/config/DataInitializer.java
+++ src/main/java/org/ardian/librarymanagementsystem/config/DataInitializer.java
@@ -6,6 +6,7 @@ import org.ardian.librarymanagementsystem.model.Role;
 import org.ardian.librarymanagementsystem.repository.LibraryUserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
  */
 
 @Slf4j
+@Profile("!test")
 @Component
 public class DataInitializer implements CommandLineRunner {
 
